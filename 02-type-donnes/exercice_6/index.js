c9e09b6a50a609549ea9fb025a19a0ca900d82ab
Benji <benjaminkabeya28@gmail.com>
2025-05-15 01:11:53 +0100

    Fin de l'exercice

--- 02-type-donnes/exercice_6/index.js
+++ 02-type-donnes/exercice_6/index.js
@@ -1,7 +1,7 @@
-let age;
-let nom;
-let estConnecte;
-let valeurNulle;
+let age = 17;
+let nom = "Benji";
+let estConnecte = true;
+let valeurNulle = null;
 let inconnue;
 
 // Les lignes ne doivent pas être modifiées
