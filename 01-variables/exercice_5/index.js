1bb96d865c8d0aa03bfa1f16092598d87f4fecdb
Benji <benjaminkabeya28@gmail.com>
2025-05-15 12:22:32 +0100

    Ajustement des exercices 2 et 5

--- 01-variables/exercice_5/index.js
+++ 01-variables/exercice_5/index.js
@@ -4,7 +4,9 @@ let celsius = 35;
 // Conversion Celsius à Fahrenheit
 function celsiusToFahrenheit(celsius) {
   // ecrire le code de conversion ici
-  let fahrenheit = (celsius * 9/5)+32;
+  let fahrenheit = (celsius * 9/5) + 32;
+
+  console.log(fahrenheit)
   // la ligne suivante ne doit pas être modifiée
   return fahrenheit;
 }
